4247de00c1bf438363797cc2939407b2843d5b7c
Yihui Xie <xie@yihui.name>
2016-02-04 13:11:46 -0600

    create .cmdargs in a local environment instead of polluting the global environment, and knit(envir = globalenvir())
    I don't think we still need the trick is.null(formals(knit)$encoding); it was there because the very early versions of knit() did not have the encoding argument, and I don't think any people are still using knitr from three years ago
    
    http://www.lyx.org/trac/ticket/7802

--- scripts/lyxknitr.R
+++ scripts/lyxknitr.R
@@ -20,21 +20,17 @@
 ## $$r path to the original input file (the lyx document)
 ## $$e encoding (e.g. 'UTF-8')
 
-library(knitr)
-.cmdargs = commandArgs(TRUE)
+local({
+  library(knitr)
+  .cmdargs = commandArgs(TRUE)
 
-## the working directory is the same with the original .lyx file; you
-## can put your data files there and functions like read.table() can
-## work correctly without specifying the full path
-setwd(.cmdargs[4])
-opts_knit$set(root.dir = getwd())
+  ## the working directory is the same with the original .lyx file; you
+  ## can put your data files there and functions like read.table() can
+  ## work correctly without specifying the full path
+  setwd(.cmdargs[4])
+  opts_knit$set(root.dir = getwd())
 
-## run knit() to get .tex or .R
-if (is.null(formals(knit)$encoding)) {
-  .orig.enc = getOption("encoding")
-  options(encoding = .cmdargs[3])
-  knit(.cmdargs[1], output = .cmdargs[2], tangle = 'tangle' %in% .cmdargs)
-} else {
+  ## run knit() to get .tex or .R
   knit(.cmdargs[1], output = .cmdargs[2], encoding = .cmdargs[3],
-       tangle = 'tangle' %in% .cmdargs)
-}
+       tangle = 'tangle' %in% .cmdargs, envir = globalenv())
+})
